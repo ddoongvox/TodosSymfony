ff1cf27ed9b06255facd6c31fd806b05d076250c
Dong Vo <dongvnh@smartosc.com>
2019-09-20 18:20:37 +0700

    part 2

---
+++ src/Controller/TaskController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Task;
+use phpDocumentor\Reflection\Types\This;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TaskController extends Controller {
+    /**
+     * @Route("/task", name="task_list")
+     */
+    public function taskList(Request $request) {
+        $taskList = $this->getDoctrine()
+            ->getRepository(Task::class)
+            ->findAll();
+        //var_dump($taskList);die();
+        return $this->render("task/index.html.twig", [
+            'taskList' => $taskList
+        ]);
+    }
+
+    /**
+     * @Route("/task/create", name="create_task")
+     */
+    public function createTask(Request $request) {
+        $task = new Task();
+        $form = $this->createFormBuilder($task)
+            ->add('name', TextType::class, ['attr' => [
+                'class' => 'form-control',
+                'style' => 'margin-bottom: 15px'
+            ]])
+            ->add('priority', ChoiceType::class, [
+                'choices' => [
+                    'Low' => 'Low',
+                    'Normal' => 'Normal',
+                    'High' => 'High'
+                ],
+                'attr' => [
+                    'class' => 'form-control',
+                    'style' => 'margin-bottom: 15px'
+            ]])
+            ->add('due_date', DateTimeType::class, [ 'attr' => [
+//                'class' => 'form-control',
+                'style' => 'margin-bottom: 15px; display: flex;'
+            ]])
+            ->add('save', SubmitType::class, [
+                'label' => 'Create Task', 'attr' => [
+                    'class' => 'btn btn-primary',
+                    'style' => 'margin-bottom: 15px'
+            ]])
+            ->getForm();
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Get data from form
+            $name = $form['name']->getData();
+            $priority = $form['priority']->getData();
+            $due_date = $form['due_date']->getData();
+            $now = new\DateTime('now');
+
+            $task->setName($name);
+            $task->setPriority($priority);
+            $task->setDueDate($due_date);
+            $task->setCreateDate($now);
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($task);
+            $em->flush();
+
+            $this->addFlash(
+                'notice',
+                'Task Create'
+            );
+            return $this->redirectToRoute('task_list');
+        }
+        return $this->render("task/create.html.twig", [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/task/update/{id}", name="update_task")
+     */
+    public function updateTask(Request $request) {
+        return $this->render("task/update.html.twig");
+    }
+
+    /**
+     * @Route("/task/delete/{id}", name="delete_task")
+     */
+    public function deleteTask(Request $request) {
+        return $this->render("task/delete.html.twig");
+    }
+
+    /**
+     * @Route("/task/details/{id}", name="details_task")
+     */
+    public function detailsTask(Request $request) {
+        return $this->render("task/details.html.twig");
+    }
+}
---
+++ src/Entity/Tags.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\TagsRepository")
+ */
+class Tags
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $td_task;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getTdTask(): ?int
+    {
+        return $this->td_task;
+    }
+
+    public function setTdTask(int $td_task): self
+    {
+        $this->td_task = $td_task;
+
+        return $this;
+    }
+}
